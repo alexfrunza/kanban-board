1fd58d76220b1005fc1bf63a16a899fa3cee5d2f
Alexandru Frunza <frunza.alexandru.2004@gmail.com>
2021-11-13 10:50:20 +0200

    Initial create-react-app

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { render } from "react-dom";
+
+const App = () => {
+    return (
+        <div>
+            Hello world
+        </div>
+    );
+};
+
+render(
+    <App />,
+    document.getElementById("root")
+);
