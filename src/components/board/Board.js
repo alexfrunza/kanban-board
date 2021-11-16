8e462efc69d5673cc87600018987bae63dfc07b8
Alexandru Frunza <frunza.alexandru.2004@gmail.com>
2021-11-16 20:30:42 +0200

    Add absolute imports

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,10 +1,108 @@
 import React, { useState, useEffect } from 'react';
-import './Board.css';
+import Column from 'components/column/Column';
+import 'components/board/Board.css';
+
+const mockBoard = {
+    name: "First Board",
+    id: "123412348",
+    users: [
+        {
+            username: "alexandru", 
+            id: "1689623",
+            imgUrl: "https://avatarfiles.alphacoders.com/233/233054.jpg"
+        },
+        {
+            username: "alexfrunza",
+            id: "2341",
+            imgUrl: "https://avatarfiles.alphacoders.com/233/233054.jpg"
+        }
+    ],
+    columns: [
+        {
+            name: "My first column",
+            id: "12341234",
+            cards: [
+                {
+                    "name": "My first card",
+                    "description": "bla bla",
+                    id: "8383",
+                    labels: [
+                        {
+                            name: "bug",
+                            id: "12349",
+                            color: "red"
+                        }
+                    ],
+                    assignedTo: [
+                        "2341"
+                    ],
+                },
+                {
+                    "name": "My second card",
+                    "description": "bla bla",
+                    id: "8383",
+                    labels: [
+                        {
+                            name: "bug",
+                            id: "12349",
+                            color: "red"
+                        }
+                    ],
+                    assignedTo: [
+                        "2341"
+                    ],
+                }
+            ]},{
+                name: "My second column it's amazing",
+                id: "12341234",
+                cards: [
+                    {
+                        "name": "My third card",
+                        "description": "bla bla",
+                        id: "8383",
+                        labels: [
+                            {
+                                name: "bug",
+                                id: "12349",
+                                color: "red"
+                            }
+                        ],
+                        assignedTo: [
+                            "2341"
+                        ],
+                    },
+                    {
+                        "name": "My forth card",
+                        "description": "bla bla",
+                        id: "1511",
+                        labels: [
+                            {
+                                name: "bug",
+                                id: "12349",
+                                color: "red"
+                            }
+                        ],
+                        assignedTo: [
+                            "2341"
+                        ],
+                    }
+                ]}
+    ]}
 
 const Board = () => {
     const [boardName, setBoardName] = useState("My first board");
     const [boardNameEdit, setBoardNameEdit] = useState(false);
     const [searchString, setSearchString] = useState('');
+
+    const [columns, setColumns] = useState([]);
+
+    // Fetch the data from API
+    useEffect(() => {
+        // TODO: fetch the data from API
+
+        setBoardName(mockBoard.name);
+        setColumns(mockBoard.columns);
+    }, []);
 
     const submitBoardName = (event) => {
         event.preventDefault();
@@ -31,26 +129,31 @@ const Board = () => {
         if(!boardNameEdit) {
             return (
                 <h2 className="boardName">
-                    { boardName }
-                    <button 
-                        className="editBoardTitle" 
-                        onClick={() => setBoardNameEdit(true)}
-                    >
-                        <i className="fas fa-edit"></i>
-                    </button>
-                </h2>
+                { boardName }
+                <button 
+                className="editBoardTitle" 
+                onClick={() => setBoardNameEdit(true)}
+                >
+                    <i className="fas fa-edit"></i>
+                </button>
+            </h2>
             );
         } else  {
             return (
                 <form className="boardName" onSubmit={ submitBoardName }>
-                    <input type="text" name="boardName" defaultValue={boardName} />
-                    <button
-                        className="editBoardTitle"
-                        type="submit"
-                    >
-                        <i className="far fa-save"></i>
-                    </button>
-                </form>
+                    <input 
+                type="text" 
+                name="boardName" 
+                defaultValue={boardName} 
+                autoComplete="off"
+                />
+                <button
+                className="editBoardTitle"
+                type="submit"
+                >
+                    <i className="far fa-save"></i>
+                </button>
+            </form>
             )
         }
     }
@@ -59,19 +162,27 @@ const Board = () => {
 
     return (
         <main className='board'>
-            { boardNameTag() }
-            <form 
-                className="searchInBoard" 
-                onSubmit={(event) => event.preventDefault()}>
-                <input 
-                    type="text"
-                    name="search" 
-                    value={ searchString }
-                    onChange={(event) => setSearchString(event.target.value)}
-                />
-                <i className="fas fa-search"></i>
-            </form>
-        </main>
+        { boardNameTag() }
+        <form 
+        className="searchInBoard" 
+        onSubmit={(event) => event.preventDefault()}>
+        <input 
+        autoComplete="off"
+        type="text"
+        name="search" 
+        value={ searchString }
+        onChange={(event) => setSearchString(event.target.value)}
+        />
+        <i className="fas fa-search"></i>
+    </form>
+    <section className="columnsSection">
+        { columns.map((data) => {
+            return <Column value={ data }/>
+        }
+        )
+        }
+    </section>
+</main>
     );
 }
 
